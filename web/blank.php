7cd22db221e99a902b7f624dc3cadda6c983c24d
xiaodong <28111600@qq.com>
2017-09-02 15:42:28 +0800

    增加 blank.php

---
+++ web/blank.php
@@ -0,0 +1,37 @@
+<?php
+require_once '../template/main.php';
+require_once '../template/head.php';
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>页面标题
+            <small>Page Title</small>
+        </h1>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Title</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        Title
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<?php
+require_once '../template/footer.php'; ?>
