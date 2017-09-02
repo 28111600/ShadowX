a059e5c513625f0e19b39e0a6e90a1a2e7739902
xiaodong <28111600@qq.com>
2017-09-02 09:14:02 +0800

    初始化静态资源

---
+++ asset/js/app.js
@@ -0,0 +1,113 @@
+!(function() {
+    var gmtHours = -((new Date()).getTimezoneOffset() / 60);
+    $.cookie('timezone', gmtHours, { expires: 30, path: '/' });
+})();
+
+var getSize = function(size, fixed) {
+    var K = 1024; //1024;
+    size = parseInt(size || 0);
+    var unit = ["B", "KB", "MB", "GB"];
+
+    var j = 0;
+    for (var i = 0; i < unit.length; i++) {
+        if (size >= Math.pow(K, i) - 1) {
+            j = i;
+        }
+    }
+    var result = (size / Math.pow(K, j));
+    if (fixed) {
+        result = result.toFixed(j === 0 ? 0 : 2);
+    }
+    return result + " " + unit[j];
+}
+
+var showUsage = function(selector, from, to, step) {
+    var options = {
+        layout: {
+            padding: {
+                left: 2,
+                right: 2,
+                top: 2,
+                bottom: 2
+            }
+        },
+        tooltips: {
+            enabled: false
+        },
+        legend: {
+            display: false
+        },
+        elements: {
+            line: {
+                tension: 0, // disables bezier curves
+            },
+            point: {
+                hoverRadius: 1.5,
+            }
+        },
+        scales: {
+            yAxes: [{
+                display: false,
+                ticks: {
+                    beginAtZero: true,
+                    display: false,
+                },
+                gridLines: {
+                    display: false
+                }
+            }],
+            xAxes: [{
+                display: false,
+                ticks: {
+                    beginAtZero: false,
+                    display: false,
+                },
+                gridLines: {
+                    display: false,
+                }
+            }]
+        },
+        responsive: false,
+        animation: {
+            duration: 0, // general animation time
+        },
+        hover: {
+            animationDuration: 0, // duration of animations when hovering an item
+        },
+        responsiveAnimationDuration: 0, // animation duration after a resize
+    };
+
+    $(selector).each(function(index, item) {
+        var ctx = $(item);
+        var data = ctx.data("value");
+        var usage = {};
+        var usage_u = [];
+        var usage_d = [];
+        var labels = [];
+        for (var i = 0; i < data.length; i++) {
+            usage[data[i].t] = data[i];
+        }
+        var t = 0;
+        for (var i = from; i <= to; i += step) {
+            usage_u.push(usage[i] ? usage[i].u : 0);
+            usage_d.push(usage[i] ? usage[i].d : 0);
+            labels.push(t++);
+        }
+        var datasets = [{
+            lineTension: 0,
+            data: usage_d,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+            pointRadius: 0.2
+        }];
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: datasets
+            },
+            options: options
+        });
+    });
+}
