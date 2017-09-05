a59077bf3753bd4c2b5e87d3987cf6dcf13b2f13
xiaodong <28111600@qq.com>
2017-09-05 21:11:36 +0800

    优化节点信息格式

--- web/ajax/node-url.php
+++ web/ajax/node-url.php
@@ -3,14 +3,9 @@ require_once '../../template/main.php';
 
 $id = $_GET['id'];
 $node = new ShadowX\Node($id);
-$server =  $node->getServer();
-$method = $node->getMethod();
-$pass = $User->getSsPasswd();
-$port = $User->getPort();
+$result['server'] = $node->getServer();
+$result['server_port'] = $User->getPort();
+$result['password'] = $User->getSsPasswd();
+$result['method'] = $node->getMethod();
+echo json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 ?>
-{
-    "server":"<?php echo $server; ?>",
-    "server_port":<?php echo $port; ?>,
-    "password":"<?php echo $pass; ?>",
-    "method":"<?php echo $method; ?>"
-}
