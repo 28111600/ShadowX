e45d9dcf9b5104538b2da687157a1e578e9006de
xiaodong <28111600@qq.com>
2017-09-07 22:22:19 +0800

    增加chart页面

---
+++ web/chart.php
@@ -0,0 +1,77 @@
+<?php
+require_once '../template/main.php';
+require_once '../template/head.php';
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>页面标题
+            <small>Page Title</small>
+        </h1>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-xs-12 col-lg-6">
+                <div class="btn-group">
+                    <a class="btn btn-default" href="#>">链接1</a>
+                    <a class="btn btn-default" href="#>">链接2</a>
+                </div>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-primary">按钮</button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-9 col-lg-8">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Title</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="usage-box"><canvas width="16px" height="9px" class="usage"></canvas></div>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<?php
+require_once '../template/footer.php'; ?>
+
+<!-- Chart 2.6.0 -->
+<script src="asset/js/Chart.bundle.min.js"></script>
+
+<script>
+    !(function() {
+        var interval = 3600 * 24;
+        var to = getTimePoint(new Date(), interval);
+        var from = to - 3600 * 24 * 30;
+
+        $(".usage").each(function() {
+            var elem = this;
+            $.ajax({
+                url: "ajax/log.php",
+                cache: false,
+                type: "POST",
+                data: {
+                    action: "getLogRange",
+                    from: from,
+                    to: to,
+                    type: "days"
+                }
+            }).done(function(text) {
+                var data = JSON.parse(text);
+                showChart(elem, from, to, interval, data.data);
+            });
+        });
+    })();
+</script>
